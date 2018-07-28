7ae623d80acd967157b7cf59d3a0490c556621b5
Sarathkumarmohan <sarath.k.mohan@student.fh-kiel.de>
2018-07-28 19:36:42 +0200

    project setup

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+
+ReactDom.render(
+
+document.getElementById("main")
+);
